c285022827871d78050c6fbe7bdd347ff42b184f
Dheeraj <dhirajmaurya345@gmail.com>
2023-03-14 17:24:44 +0530

    improved

--- controller/toureController.js
+++ controller/toureController.js
@@ -207,23 +207,5 @@ exports.getMonthlyPlan = async (req, res) => {
     });
   }
 };
-exports.patchToursById = async (req, res) => {
-  try {
-    const tour = await Tours.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidator: true,
-    });
-    res.status(200).json({
-      status: "success",
-      data: {
-        tour,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: "Failed",
-      message: err,
-    });
-  }
-};
+
 
